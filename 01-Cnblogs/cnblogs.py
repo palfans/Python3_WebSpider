ed855d3237c58df4c1a15114afd4bfd8e60515f6
palfans <palfans@gmail.com>
2019-03-13 17:42:18 -0500

    feat: update mongodb address and cnblogs page url

--- 01-Cnblogs/cnblogs.py
+++ 01-Cnblogs/cnblogs.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-
 """
 Updated at 23:57 at Nov 17,2018
 @title: 使用urllib库爬取博客园文章并存储至MongoDB
@@ -16,9 +15,10 @@ import time
 init(autoreset=True)
 
 # 创建数据库链接
-client = pymongo.MongoClient('localhost')
+client = pymongo.MongoClient('192.168.1.80')
 db = client['cnblogs']
 collection = db['home']
+
 
 def get_data(url, page):
     """
@@ -31,7 +31,8 @@ def get_data(url, page):
         headers = {
             'origin': 'https://www.cnblogs.com',
             'referer': 'https://www.cnblogs.com/',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+            'user-agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
             'x-requested-with': 'XMLHttpRequest',
         }
 
@@ -47,7 +48,8 @@ def get_data(url, page):
         # 编码
         data = bytes(parse.urlencode(payload), encoding='utf-8')
         # 构建Request类
-        _request = request.Request(url=url, headers=headers, data=data, method='POST')
+        _request = request.Request(
+            url=url, headers=headers, data=data, method='POST')
     else:
         _request = request.Request(url=url)
 
@@ -61,6 +63,7 @@ def get_data(url, page):
     except error.URLError as e:
         print(e.reason)
         return None
+
 
 def parse_data(response):
     """
@@ -78,24 +81,34 @@ def parse_data(response):
     for div in divs:
         _data = {
             # 文章标题
-            'title': div.find('.titlelnk').text(),
+            'title':
+            div.find('.titlelnk').text(),
             # 文章详情页链接
-            'url': div.find('.titlelnk').attr('href'),
+            'url':
+            div.find('.titlelnk').attr('href'),
             # 文章简介
-            'short': div.find('.post_item_summary').text(),
+            'short':
+            div.find('.post_item_summary').text(),
             # 博主
-            'author': div.find('.post_item_foot .lightblue').text(),
+            'author':
+            div.find('.post_item_foot .lightblue').text(),
             # 博主的主页链接
-            'author_url': div.find('.post_item_foot .lightblue').attr('href'),
+            'author_url':
+            div.find('.post_item_foot .lightblue').attr('href'),
             # 发布时间
-            'create_time': div.find('.post_item_foot').text().split()[2] + ' ' + div.find('.post_item_foot').text().split()[3],
+            'create_time':
+            div.find('.post_item_foot').text().split()[2] + ' ' +
+            div.find('.post_item_foot').text().split()[3],
             # 评论数
-            'comment_counts': div.find('.article_comment .gray').text(),
+            'comment_counts':
+            div.find('.article_comment .gray').text(),
             # 阅读量
-            'read_counts': div.find('.article_view .gray').text(),
+            'read_counts':
+            div.find('.article_view .gray').text(),
         }
         data.append(_data)
     return data
+
 
 def save_data(data):
     """
@@ -103,7 +116,8 @@ def save_data(data):
     :param data: 解析的数据
     :return: None
     """
-    collection.insert(data)
+    collection.insert_many(data)
+
 
 def main(url, page=''):
     """
@@ -118,14 +132,18 @@ def main(url, page=''):
     # 存储数据
     save_data(data)
 
+
 if __name__ == '__main__':
-    print(Fore.RED + '提示：本次抓取范围仅限博客园最新文章, 测试仅抓取前10页数据！\n')
+    total = 2
+    print(Fore.RED + '提示：本次抓取范围仅限博客园最新文章, 测试仅抓取前' + str(total) + '页数据！\n')
     global url
-    for i in tqdm(range(10), desc='抓取进度', ncols=100):
+    for i in tqdm(range(total), desc='抓取进度', ncols=100):
         if i == 0:
             url = 'https://www.cnblogs.com/'
             main(url)
         else:
-            url = 'https://www.cnblogs.com/mvc/AggSite/PostList.aspx'
-            main(url, i+1)
+            page = i + 1
+            url = 'https://www.cnblogs.com/sitehome/p/' + str(page)
+            #main(url, i + 1)
+            main(url)
         time.sleep(1)
